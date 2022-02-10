4bd5614af80fa086c6feba719b3a22ccb69946ea
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-02-11 02:55:55 +0800

    megaparsack has function-lib dep

--- info.rkt
+++ info.rkt
@@ -4,8 +4,7 @@
                "curly-fn-lib"
                "try-catch-finally-lib"
                ; parser
-               "megaparsack-lib"
-               "functional-lib"))
+               "megaparsack-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "Description Here")
 (define version "0.0")
