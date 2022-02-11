a6c4ef767ebcf2c518aa448894753023c791462b
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-02-12 02:07:03 +0800

    type auto can invoke solver
    related #2

--- main.rkt
+++ main.rkt
@@ -1,17 +1,19 @@
 #lang racket/base
 (require try-catch-finally
          "puzzle.rkt"
+         "solver.rkt"
          "color.rkt")
 
 (define (gameloop)
   (define p (generate-puzzle))
   (let loop ()
-    (printf "~a > " (puzzle-compute-answer p))
-    (define solution-string (read-line))
+    (printf "(~a)> " (puzzle-compute-answer p))
+    (define input (read-line))
     (cond
-      [(eof-object? solution-string) (displayln "  ")]
+      [(eof-object? input) (displayln "  ")]
+      [(equal? "auto" input) (solve p)]
       [else
-       (try (define r (answer p solution-string))
+       (try (define r (answer p input))
             (displayln (pretty r))
             (cond
               [(solved? r) (displayln "solved")]
--- solver.rkt
+++ solver.rkt
@@ -1,15 +1,20 @@
 #lang racket
+(provide solve)
+
 (require racket/random
          try-catch-finally
          "puzzle.rkt"
          "color.rkt")
 
-(define s1 (list->mutable-set (string->list "123456789")))
-(define s2 (list->mutable-set (string->list "01234+-*/56789")))
-(define s3 (list->mutable-set (string->list "01234+-*/56789")))
-(define s4 (list->mutable-set (string->list "01234+-*/56789")))
-(define s5 (list->mutable-set (string->list "01234+-*/56789")))
-(define s6 (list->mutable-set (string->list "0123456789")))
+(define (solve p)
+  (define-values (generate-solution learn) (generate-solver))
+  (let loop ()
+    (define r (answer p (generate-solution (puzzle-compute-answer p))))
+    (displayln (pretty r))
+    (learn r)
+    (if (solved? r)
+        (void)
+        (loop))))
 
 (define (handle-tmp-set s pp)
   (define new-s (cond
@@ -22,57 +27,59 @@
       (random-ref s)
       (random-ref new-s)))
 
-(define (generate-solution result)
-  (define p1 (random-ref s1))
-  (define p2 (random-ref s2))
-  (define p3 (handle-tmp-set s3 p2))
-  (define p4 (handle-tmp-set s4 p3))
-  (define p5 (handle-tmp-set s5 p4))
-  (define p6 (if (eq? p5 #\/)
-                 (random-ref (set-remove (list->set (set->list s6)) #\0))
-                 (random-ref s6)))
-  (define solution (string p1 p2 p3 p4 p5 p6))
-  (try
-   (if (= result (calculate solution))
-       solution
-       (generate-solution result))
-   (catch (e)
-          (generate-solution result))))
+(define (generate-solver)
+  (define s1 (list->mutable-set (string->list "123456789")))
+  (define s2 (list->mutable-set (string->list "01234+-*/56789")))
+  (define s3 (list->mutable-set (string->list "01234+-*/56789")))
+  (define s4 (list->mutable-set (string->list "01234+-*/56789")))
+  (define s5 (list->mutable-set (string->list "01234+-*/56789")))
+  (define s6 (list->mutable-set (string->list "0123456789")))
 
-(define (update-set response)
-  (for ([p response]
-        [i (in-range 0 6)])
-    (match-define (cons c status) p)
-    (case status
-      [(gray)
-       (set-remove! s1 c)
-       (set-remove! s2 c)
-       (set-remove! s3 c)
-       (set-remove! s4 c)
-       (set-remove! s5 c)
-       (set-remove! s6 c)]
-      [(green) (case i
-                 [(0) (set! s1 (mutable-set c))]
-                 [(1) (set! s2 (mutable-set c))]
-                 [(2) (set! s3 (mutable-set c))]
-                 [(3) (set! s4 (mutable-set c))]
-                 [(4) (set! s5 (mutable-set c))]
-                 [(5) (set! s6 (mutable-set c))])]
-      [(yellow) (case i
-                  [(0) (set-remove! s1 c)]
-                  [(1) (set-remove! s2 c)]
-                  [(2) (set-remove! s3 c)]
-                  [(3) (set-remove! s4 c)]
-                  [(4) (set-remove! s5 c)]
-                  [(5) (set-remove! s6 c)])])))
+  (define (generate-solution result)
+    (define p1 (random-ref s1))
+    (define p2 (random-ref s2))
+    (define p3 (handle-tmp-set s3 p2))
+    (define p4 (handle-tmp-set s4 p3))
+    (define p5 (handle-tmp-set s5 p4))
+    (define p6 (if (eq? p5 #\/)
+                   (random-ref (set-remove (list->set (set->list s6)) #\0))
+                   (random-ref s6)))
+    (define solution (string p1 p2 p3 p4 p5 p6))
+    (try
+     (if (= result (calculate solution))
+         solution
+         (generate-solution result))
+     (catch (e)
+            (generate-solution result))))
+
+  (define (learn response)
+    (for ([p response]
+          [i (in-range 0 6)])
+      (match-define (cons c status) p)
+      (case status
+        [(gray) (set-remove! s1 c)
+                (set-remove! s2 c)
+                (set-remove! s3 c)
+                (set-remove! s4 c)
+                (set-remove! s5 c)
+                (set-remove! s6 c)]
+        [(green) (case i
+                   [(0) (set! s1 (mutable-set c))]
+                   [(1) (set! s2 (mutable-set c))]
+                   [(2) (set! s3 (mutable-set c))]
+                   [(3) (set! s4 (mutable-set c))]
+                   [(4) (set! s5 (mutable-set c))]
+                   [(5) (set! s6 (mutable-set c))])]
+        [(yellow) (case i
+                    [(0) (set-remove! s1 c)]
+                    [(1) (set-remove! s2 c)]
+                    [(2) (set-remove! s3 c)]
+                    [(3) (set-remove! s4 c)]
+                    [(4) (set-remove! s5 c)]
+                    [(5) (set-remove! s6 c)])])))
+
+  (values generate-solution learn))
 
 (module+ main
   (define test-target (generate-puzzle))
-
-  (let loop ()
-    (define r (answer test-target (generate-solution (puzzle-compute-answer test-target))))
-    (displayln (pretty r))
-    (update-set r)
-    (if (solved? r)
-        (void)
-        (loop))))
+  (solve test-target))
