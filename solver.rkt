87839dfadc3ad2cef1666a0029a43f31fc7bb633
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-02-11 18:32:08 +0800

    simple solver!

--- solver.rkt
+++ solver.rkt
@@ -1,13 +1,15 @@
 #lang racket
 (require racket/random
-         "puzzle.rkt")
+         "puzzle.rkt"
+         "color.rkt")
 
 (define test-target (puzzle "60/5*9" 108))
 
+(define s (list->mutable-set (string->list "123456789")))
+(define s2 (list->mutable-set (string->list "01234+-*/56789")))
+(define s3 (list->mutable-set (string->list "0123456789")))
+
 (define (generate-solution result)
-  (define s (string->list "123456789"))
-  (define s2 (string->list "01234+-*/56789"))
-  (define s3 (string->list "0123456789"))
   (define p1 (random-ref s))
   (define p2 (random-ref s2))
   (define p3 (if (member p2 (string->list "+-*/")) (random-ref s) (random-ref s2)))
@@ -19,6 +21,21 @@
       solution
       (generate-solution result)))
 
-(answer test-target (generate-solution 108))
-(answer test-target (generate-solution 108))
-(answer test-target (generate-solution 108))
+(define (update-set response)
+  (for ([p response])
+    (match-define (cons c status) p)
+    (case status
+      [(gray)
+       (set-remove! s c)
+       (set-remove! s2 c)
+       (set-remove! s3 c)
+       ]
+      [else (void)])))
+
+(let loop ()
+  (define r (answer test-target (generate-solution 108)))
+  (displayln (pretty r))
+  (update-set r)
+  (if (solved? r)
+      (void)
+      (loop)))
