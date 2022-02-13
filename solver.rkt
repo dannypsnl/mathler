d0ed5c45d6e8f307f1e409c2ca2c49c43dd0ea5c
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-02-14 03:37:45 +0800

    solver: reject solution didn't contain yellows
    resolve #4

--- solver.rkt
+++ solver.rkt
@@ -25,6 +25,10 @@
       [else s]))
   (if (set-empty? new-s) (random-ref s) (random-ref new-s)))
 
+(define (yellow-in? s yellow*)
+  (for/and ([c yellow*])
+    (string-contains? s (string c))))
+
 (define (generate-solver)
   (define s1 (list->mutable-set (string->list "123456789")))
   (define s2 (list->mutable-set (string->list "01234+-*/56789")))
@@ -32,6 +36,7 @@
   (define s4 (list->mutable-set (string->list "01234+-*/56789")))
   (define s5 (list->mutable-set (string->list "01234+-*/56789")))
   (define s6 (list->mutable-set (string->list "0123456789")))
+  (define yellow* (mutable-set))
 
   (define (generate-solution result)
     (define p1 (random-ref s1))
@@ -42,7 +47,9 @@
     (define p6
       (if (eq? p5 #\/) (random-ref (set-remove (list->set (set->list s6)) #\0)) (random-ref s6)))
     (define solution (string p1 p2 p3 p4 p5 p6))
-    (try (if (= result (calculate solution)) solution (generate-solution result))
+    (try (if (and (= result (calculate solution)) (yellow-in? solution yellow*))
+             solution
+             (generate-solution result))
          (catch (e) (generate-solution result))))
 
   (define (learn response)
@@ -65,6 +72,7 @@
            [(4) (set! s5 (mutable-set c))]
            [(5) (set! s6 (mutable-set c))])]
         [(yellow)
+         (set-add! yellow* c)
          (case i
            [(0) (set-remove! s1 c)]
            [(1) (set-remove! s2 c)]
