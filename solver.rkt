6294781a999f21ac57c6870637ff4c44710b8a52
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-02-14 02:57:44 +0800

    solver bug: rules
    1. +-* 0 is ok
    2. +-* 0 <num> is bad
    3. / 0 is impossible, whatever follows <num> or not

--- solver.rkt
+++ solver.rkt
@@ -15,12 +15,13 @@
     (learn r)
     (if (solved? r) (void) (loop))))
 
-(define (handle-tmp-set s pp)
+(define (handle-tmp-set s p1 p2)
   (define new-s
     (cond
-      [(string-contains? "+-*" (string pp))
-       (set-subtract (list->set (set->list s)) (set #\+ #\- #\* #\/ #\0))]
-      [(eq? pp #\/) (set-subtract (list->set (set->list s)) (set #\+ #\- #\* #\/ #\0))]
+      [(string-contains? "+-*" (string p1))
+       (set-subtract (list->set (set->list s)) (set #\+ #\- #\* #\/))]
+      [(eq? p1 #\/) (set-subtract (list->set (set->list s)) (set #\+ #\- #\* #\/ #\0))]
+      [(and (string-contains? "+-*/" (string p1)) (eq? p2 #\0)) (set #\+ #\- #\* #\/)]
       [else s]))
   (if (set-empty? new-s) (random-ref s) (random-ref new-s)))
 
@@ -35,9 +36,9 @@
   (define (generate-solution result)
     (define p1 (random-ref s1))
     (define p2 (random-ref s2))
-    (define p3 (handle-tmp-set s3 p2))
-    (define p4 (handle-tmp-set s4 p3))
-    (define p5 (handle-tmp-set s5 p4))
+    (define p3 (handle-tmp-set s3 p2 p1))
+    (define p4 (handle-tmp-set s4 p3 p2))
+    (define p5 (handle-tmp-set s5 p4 p3))
     (define p6
       (if (eq? p5 #\/) (random-ref (set-remove (list->set (set->list s6)) #\0)) (random-ref s6)))
     (define solution (string p1 p2 p3 p4 p5 p6))
