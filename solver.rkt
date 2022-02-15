87f2b7412a08a02f9043cab225cecf34e001c301
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-02-15 16:11:33 +0800

    solve bug: p6 didn't reject `+07`

--- solver.rkt
+++ solver.rkt
@@ -45,7 +45,10 @@
     (define p4 (handle-tmp-set s4 p3 p2))
     (define p5 (handle-tmp-set s5 p4 p3))
     (define p6
-      (if (eq? p5 #\/) (random-ref (set-remove (list->set (set->list s6)) #\0)) (random-ref s6)))
+      (cond
+        [(eq? p5 #\/) (random-ref (set-remove (list->set (set->list s6)) #\0))]
+        [(and (string-contains? "+-*/" (string p4)) (eq? p5 #\0)) (set #\+ #\- #\* #\/)]
+        [else (random-ref s6)]))
     (define solution (string p1 p2 p3 p4 p5 p6))
     (try (if (and (= result (calculate solution)) (yellow-in? solution yellow*))
              solution
